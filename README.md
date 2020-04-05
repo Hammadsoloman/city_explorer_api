1892f242f448e7c438974bb12bd8a02a01304ef0
Hammadsoloman <hammadali95@yahoo.com>
2020-04-06 01:10:33 +0300

    end feature 1+2+4 and still work in som thing in the feature 3

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # city_explorer_api
+
+Number and name of feature: 1. Repository Set Up
+
+Estimate of time needed to complete: 0.5
+
+
+Actual time needed to complete: 0.45-1
+
+*************************************************************************************
+
+Number and name of feature: 2. Locations
+
+Estimate of time needed to complete: 1
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete: 2 // i repeat all things alone
+
+************************************************************************************
+Number and name of feature: 3. Weathe
+
+Estimate of time needed to complete: 1.5
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete: 2 //still work in it    
+
+************************************************************************************
+Number and name of feature: 4. Errors
+
+Estimate of time needed to complete: 0.10
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete: 0.10
+
+
+
+# Project Name
+
+**Author**: ## Hammad Ali
+**Version**: 6.13.4
+
+## Overview
+
+**We use this application, that when the user inter the name of the city we show gim the map and the weather and the date**
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+1. Repository Set Up.
+2. End the Location part.
+3. End the weather part
+4. messages if something goes wrong 
+
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+**i use express library ,cons,dotenv.and the labguage for the app is the englisg**
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+**Still in progress**
---
+++ server.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/*to get the library, and express function has alots of methods and 
+properties can we use it in the server*/
+const express=require('express');
+
+//to get promision who can touch my server
+// Load Environment Variables from the .env file
+const cors =require('cors');
+
+//read our invironment variable
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+const app=express();
+
+app.use(cors(/*in this lab its open for every one*/));
+
+//now we should handle the route to the port, and that by using get
+app.get('/',(request,response)=>{
+    response.status(200).send('Welcome');//done successfully 
+});
+
+
+//we send the request to the express server, and our case have it in JSON file, not from APi 
+
+server.get('/location',(request,response)=>{
+    //get all of the data from json file
+    try {
+    const geoData = require('./data/geo.json');
+    const city = request.query.city;
+    //we use the query to access the parameter that we nees
+    const locationData= new Location(city,geoData) //location data is an object
+    response.status(200).json(locationData);
+    } catch (error) {
+    errorHandler(error, request, response);
+  }
+    response.send(locationData);
+    // consle.log(locationDate);
+});
+
+
+app.get('/weather',(request,response)=>{
+  try {
+  const city = request.query.city;
+  const sky=require('./data/darksky.json');
+  const locationWeather= new Weather(city,sky)
+  response.status(200).json(locationWeather);
+    } catch (error) {
+    errorHandler(error, request, response);
+  }
+    response.send(locationData);
+    // consle.log(locationDate);
+});
+
+//   console.log(sky);
+
+//we use it to select the all , and we can use "get" rather than "use"
+app.use('*', notFoundHandler);
+function Location(city,geoData){
+    this.search_query=city;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
+}
+// console.log(Location)
+
+//constructor for the weather, when the user wrote the city it will show the weather
+app.use('*', notFoundHandler);
+function Weather(city,sky){
+    this.search_query=city;
+    this.description = sky[0].weather.description;
+    this.time = sky[0].valid_date;
+}
+// console.log(Weather)
+
+
+
+function notFoundHandler(request, response) {
+response.status(404).send('NOT FOUND!!');
+  }
+
+
+//to handle with the error that happend with uncorrect input or value
+function errorHandler(error, request, response) {
+    response.status(500).send({'Status': 500,responseText:'sorry something went wrong'});
+
+  }
+ 
+// listening for requests
+app.listen (PORT,()=>{
+    console.log(`the server is up and running on ${PORT}`);
+});
