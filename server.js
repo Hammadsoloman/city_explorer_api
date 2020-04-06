e89e9fe749cc18257792fd2df239200d0feba403
Hammadsoloman <hammadali95@yahoo.com>
2020-04-06 13:21:56 +0300

    fix the problems

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/',(request,response)=>{
 
 //we send the request to the express server, and our case have it in JSON file, not from APi 
 
-server.get('/location',(request,response)=>{
+app.get('/location',(request,response)=>{
     //get all of the data from json file
     try {
     const geoData = require('./data/geo.json');
@@ -35,23 +35,24 @@ server.get('/location',(request,response)=>{
     } catch (error) {
     errorHandler(error, request, response);
   }
-    response.send(locationData);
-    // consle.log(locationDate);
+ 
 });
 
 
 app.get('/weather',(request,response)=>{
   try {
-  const city = request.query.city;
-  const sky=require('./data/darksky.json');
-  const locationWeather= new Weather(city,sky)
+  const darkskyDate=require('./data/darksky.json');
+  const weatherSuemmry=[];
+
+  darkskyDate.data.forEash((day,index)=>{
+   weatherSuemmry.push(new Weather (day));
+  });
   response.status(200).json(locationWeather);
     } catch (error) {
     errorHandler(error, request, response);
   }
-    response.send(locationData);
-    // consle.log(locationDate);
 });
+  
 
 //   console.log(sky);
 
@@ -62,29 +63,27 @@ function Location(city,geoData){
     this.formatted_query = geoData[0].display_name;
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
-}
+};
 // console.log(Location)
 
 //constructor for the weather, when the user wrote the city it will show the weather
-app.use('*', notFoundHandler);
-function Weather(city,sky){
-    this.search_query=city;
-    this.description = sky[0].weather.description;
-    this.time = sky[0].valid_date;
-}
+function Weather(sky){
+    this.forcast = sky.weather.description;
+    this.time = new Date(sky.valid_date).toDateString();
+};
 
 
 
 function notFoundHandler(request, response) {
 response.status(404).send('NOT FOUND!!');
-  }
+  };
 
 
 //to handle with the error that happend with uncorrect input or value
 function errorHandler(error, request, response) {
     response.status(500).send({'Status': 500,responseText:'sorry something went wrong'});
 
-  }
+  };
  
 // listening for requests
 app.listen (PORT,()=>{
